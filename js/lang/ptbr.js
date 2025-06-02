372517b6983a26fed879190867ec461c1a9bd26f
kickaasssssssss <107225784+kickaasssssssss@users.noreply.github.com>
2025-06-02 20:17:37 +0700

    Update ptbr.js

--- js/lang/ptbr.js
+++ js/lang/ptbr.js
@@ -17,8 +17,8 @@ const ptbr = {
   // Override texts if present in URL
   tiktok: {
     follow: () => `${urlParams.followText || 'followed the channel'}`,
-    share : () => `shared the live!`,
+    share: () => `${urlParams.shareText || 'shared the live!'}`,
     sub: ({ months }) => `${urlParams.subText || 'subscribed for'} <i class="fa-solid fa-star"></i> <strong>${months || 1} ${(months && months > 1) ? 'months' : 'month'}</strong>`,
-    gift: ({ gift, count, coins }) => `${urlParams.giftText || 'gifted'} <strong>${gift} x${count}</strong> (🪙 <strong>${coins} ${(coins && coins > 1) ? 'coins' : 'coin'})</strong>`,
+    gift: ({ gift, count, coins }) => `${urlParams.giftText || 'gifted'} <strong>${gift} x${count}</strong> (<strong>${coins} 🪙)</strong>`,
   }
 };
